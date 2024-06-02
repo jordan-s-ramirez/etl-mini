6888a78cd5c39d8f52b1e4d32a136142c0c81bda
Jordan Ramirez <jordan.s.ramirez@gmail.com>
2024-06-02 14:53:14 -0400

    remove trailing coment

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -34,7 +34,6 @@ export default function Layout({ children }) {
                 <Navbar drawerOpen={drawerOpen} theme={theme} />
               </div>
             </aside>
-            {console.log()}
             <main className={layoutStyle.customLayoutMain} style={{ backgroundColor: theme.palette.custom.contentBackgroundColor }}>{children}</main>
           </div>
           {/* <div className={layoutStyle.customLayoutFooter}></div> */}
